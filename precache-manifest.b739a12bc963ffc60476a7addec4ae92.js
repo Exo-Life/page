20a91c22abd53aeef452f22856188e503566ef09
simon <simondjv2016@gmail.com>
2023-10-16 23:01:32 -0500

    Updates

---
+++ precache-manifest.b739a12bc963ffc60476a7addec4ae92.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45cec1b08492b49a5f0769bcb5dd2101",
+    "url": "/page/index.html"
+  },
+  {
+    "revision": "6e1c5d9dc9b17d6efebd",
+    "url": "/page/static/css/main.c6d931dc.chunk.css"
+  },
+  {
+    "revision": "d05cd82b96f3defb4e3c",
+    "url": "/page/static/js/2.c24fb992.chunk.js"
+  },
+  {
+    "revision": "df6ea542164f30206c4d",
+    "url": "/page/static/js/3.57b001d4.chunk.js"
+  },
+  {
+    "revision": "6e1c5d9dc9b17d6efebd",
+    "url": "/page/static/js/main.56915a02.chunk.js"
+  },
+  {
+    "revision": "a6589145a47ea60846c7",
+    "url": "/page/static/js/runtime~main.cb56afc7.js"
+  },
+  {
+    "revision": "63bef7bee2b5c7fef1be75f610040a3b",
+    "url": "/page/static/media/logo.63bef7be.png"
+  },
+  {
+    "revision": "b173ebef90371a686611d59e57f7cc49",
+    "url": "/page/static/media/product_tr.b173ebef.png"
+  },
+  {
+    "revision": "f9c54ec923b522645dfb6316927ebee4",
+    "url": "/page/static/media/protesis.f9c54ec9.jpg"
+  }
+]);
